039e8c3e1bbef3d5f3db853e6a9e96bdfd04015f
ohdj <71207981+ohdj@users.noreply.github.com>
2024-01-16 15:53:13 +0800

    feat: 主世界地狱坐标相互转换显示
    在主世界的时候显示对应地狱坐标（即主世界坐标/8）
    在地狱的时候显示对应主世界坐标（即地狱坐标*8）

--- src/main/java/org/ohdj/locateme/LocateMe.java
+++ src/main/java/org/ohdj/locateme/LocateMe.java
@@ -2,12 +2,14 @@ package org.ohdj.locateme;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
@@ -25,7 +27,7 @@ public final class LocateMe extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "只有玩家可以使用这个命令！");
             return true;
@@ -39,10 +41,11 @@ public final class LocateMe extends JavaPlugin {
         double z = Math.round(player.getLocation().getZ() * 10.0) / 10.0;
 
         // 获取玩家所在的维度
+        World.Environment dimension = player.getWorld().getEnvironment();
         String dimensionInfo;
         ChatColor dimensionColor;
 
-        switch (player.getWorld().getEnvironment()) {
+        switch (dimension) {
             case NORMAL:
                 dimensionColor = ChatColor.GOLD; // 主世界橙色
                 dimensionInfo = "主世界";
@@ -56,17 +59,21 @@ public final class LocateMe extends JavaPlugin {
                 dimensionInfo = "末地";
                 break;
             default:
-                dimensionColor = ChatColor.WHITE; // 未知白色
+                dimensionColor = ChatColor.RESET; // 未知白色
                 dimensionInfo = "未知";
                 break;
         }
 
         // 在服务器公屏显示提示信息
-        Bukkit.broadcastMessage(ChatColor.AQUA + "[Here] " + ChatColor.YELLOW + player.getName() + ChatColor.WHITE + " 在 " + dimensionColor + dimensionInfo +
+        Bukkit.broadcastMessage(ChatColor.AQUA + "[Here] " + ChatColor.YELLOW + player.getName() + ChatColor.RESET + " 在 " + dimensionColor + dimensionInfo +
                 ChatColor.RESET + " 的 [ " +
                 ChatColor.RED + x + ChatColor.RESET + " , " +
                 ChatColor.GREEN + y + ChatColor.RESET + " , " +
-                ChatColor.BLUE + z + ChatColor.RESET + " ]");
+                ChatColor.BLUE + z + ChatColor.RESET + (dimension == World.Environment.NORMAL ? " ] => " + ChatColor.DARK_RED + "地狱" + ChatColor.RESET + " [ " +
+                ChatColor.RED + Math.round(x / 8 * 10.0) / 10.0 + ChatColor.RESET + " , " +
+                ChatColor.BLUE + Math.round(z / 8 * 10.0) / 10.0 + ChatColor.RESET : dimension == World.Environment.NETHER ? " ] => " + ChatColor.GOLD + "主世界" + ChatColor.RESET + " [ " +
+                ChatColor.RED + Math.round(x * 8 * 10.0) / 10.0 + ChatColor.RESET + " , " +
+                ChatColor.BLUE + Math.round(z * 8 * 10.0) / 10.0 + ChatColor.RESET : "") + " ]");
 
         // 将使用该指令的玩家添加勾边高亮效果，持续10秒
         player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 200, 1));
